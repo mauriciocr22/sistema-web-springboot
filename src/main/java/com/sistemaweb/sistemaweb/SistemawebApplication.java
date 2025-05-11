9688d644664c37cd468ed272a624f1ff20c761b1
Maurício de Carvalho <mauriciocr223@gmail.com>
2025-05-11 18:15:42 -0300

    First commit

---
+++ src/main/java/com/sistemaweb/sistemaweb/SistemawebApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaweb.sistemaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaweb/sistemaweb/SistemawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaweb.sistemaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
